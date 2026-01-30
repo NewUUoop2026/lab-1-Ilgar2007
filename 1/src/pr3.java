0af73e4c05801b8f4d3b44f1f4fe72fdfbca291a
Ilgar2007 <140079655+Ilgar2007@users.noreply.github.com>
2026-01-30 10:38:51 +0500

    Delete 1 directory

--- 1/src/pr3.java
+++
@@ -1,8 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    double area = 4.5*7.9;
-    double perimeter = 2*(4.5+7.9);
-    System.out.println("area: " + area);
-    System.out.println("perimeter: " + perimeter);
-}
